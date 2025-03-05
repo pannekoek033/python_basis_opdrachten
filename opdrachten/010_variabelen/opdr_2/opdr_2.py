7cb3969fea518cca2e2fcb55fd85457ad17f14c4
pannekoek033 <keano2602@outlook.com>
2025-03-05 15:13:57 +0100

    opdracht 2 afgemaakt

--- opdrachten/010_variabelen/opdr_2/opdr_2.py
+++ opdrachten/010_variabelen/opdr_2/opdr_2.py
@@ -1,7 +1,8 @@
-# Opdracht 1
-# Naam student:
-# Groep:
+# Opdracht 2
+# Naam student: Keano Hotzky
+# Groep:IT2B  
 
 # Hier komt je code...
 
-my_tuple = ...
+my_tuple = (11, "oh een tuple", 13.5, 8009)
+print (my_tuple)
