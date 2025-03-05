4c830b13fb8e1a1cab2ab0cd595a0493092a42b7
pannekoek033 <keano2602@outlook.com>
2025-03-05 15:02:37 +0100

    opdracht 1 afgemaakt

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,7 +1,8 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 # Hier komt je code...
 
-my_list = ...
+my_list = [13, "wat een weertje", 12.5, 8]
+print(my_list)
