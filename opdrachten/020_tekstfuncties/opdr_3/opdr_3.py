43e1ba931e5704822d1c4d6d127a678bb30c0241
pannekoek033 <keano2602@outlook.com>
2025-03-12 15:10:45 +0100

    opdracht af

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,20 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 # Hier komt je code...
+
+
+
+
+
+
+print("    *        " * 5)
+print("   ***       " * 5)
+print("  ******     " * 5)
+print(" ********    " * 5)
+print("***********  " * 5)
+print("    ***      " * 5)
+print("    ***      " * 5)
+print("    ***      " * 5)
+
