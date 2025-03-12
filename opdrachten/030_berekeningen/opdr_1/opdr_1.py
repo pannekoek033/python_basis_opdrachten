528a06c4ab039113530dbdeb77038fe978dd7db4
pannekoek033 <keano2602@outlook.com>
2025-03-12 15:21:55 +0100

    opdracht af

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,13 @@
 # Opdracht 1 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 # Hier komt je code...
+
+joe = 23
+bob = 15
+fred = 22
+
+print ("Dit weten we over joe, bob en fred:")
+print("De totale waarde van alle leeftijden bij elkaar opgeteld =",joe+bob+fred)
+print("De gemiddelde leeftijd van deze boys is:", (joe+bob+fred) // 3)
