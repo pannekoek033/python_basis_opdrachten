700b8b5dc5a3b016fee6cc0c2fd3bfa7133f4c42
pannekoek033 <keano2602@outlook.com>
2025-03-12 14:48:19 +0100

    Opdracht afgemaakt

--- opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
+++ opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
@@ -1,6 +1,6 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep:IT2B
 
 # Hier komt je code...
 
@@ -8,7 +8,7 @@
 # Zorg dat de volgende zin op het scherm wordt getoond met de print-functie:
 # O mamma mia, wat maak je toch heerlijke pizza's
 
-print()
+print("O mamma mia, wat maak je toch heerlijke pizzas")
 
 # Gebruik onderstaande variabelen en de print-functie
 # en toon de volgende zin op het scherm: Albert Einstein zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'
@@ -16,4 +16,4 @@ voornaam = "albert"
 achternaam = "einstein"
 
 
-print()
+print(voornaam.title(), achternaam.title(), "zei ooit: 'als je niks fout doet, dan kun je ook niks leren!' ")
