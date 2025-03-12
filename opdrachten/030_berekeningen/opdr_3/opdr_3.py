af6b7ac484ea8b3d5fd325b9be676ce80df69aa5
pannekoek033 <keano2602@outlook.com>
2025-03-12 15:39:02 +0100

    opdracht af

--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -1,16 +1,23 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 # Hier komt je code...
+# Functie om y te berekenen op basis van de gegeven formule
+def bereken_y(x):
+    return (4 * x**3) - (2 * x**2) - 1
+
+# Eerste testwaarde
 x = 1
-y = ...
-print( ... )
+y = bereken_y(x)
+print(f"De uitkomst is: {y}")
 
+# Tweede testwaarde
 x = 2
-y = ...
-print( ... )
+y = bereken_y(x)
+print(f"De uitkomst is: {y}")
 
+# Derde testwaarde
 x = 0
-y = ...
-print( ... )
+y = bereken_y(x)
+print(f"De uitkomst is: {y}")
