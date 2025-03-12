95a934189e606a35d08014baa074beee79444610
pannekoek033 <keano2602@outlook.com>
2025-03-12 15:31:41 +0100

    opdracht af

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -1,10 +1,23 @@
 # Opdracht 2 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 # Hier komt je code...
 
-c = ...
-f = ...
+def celsius_naar_fahrenheit(c):
+    return (c * 9/5) + 32
 
-print()
+def fahrenheit_naar_celsius(f):
+    return (f - 32) * 5/9
+
+# Eerste set waarden
+c = -12
+f = 102
+print(f"{c} graden Celsius is gelijk aan {celsius_naar_fahrenheit(c):.1f} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {fahrenheit_naar_celsius(f):.1f} graden Celsius")
+
+# Tweede set waarden
+c = 62.2
+f = 96
+print(f"{c} graden Celsius is gelijk aan {celsius_naar_fahrenheit(c):.1f} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {fahrenheit_naar_celsius(f):.1f} graden Celsius")
