2812d7b378c1c80f35e0dec6c041e085f98b266b
pannekoek033 <keano2602@outlook.com>
2025-03-26 15:02:13 +0100

    opdracht af

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -1,9 +1,15 @@
 # Opdracht 1 lists
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+mylist = []
+dict_1 = { "id": 0, "voornaam": "Keano", "achternaam": "Hotzky" }
+dict_2 = { "id": 1, "voornaam": "Silas", "achternaam": "Brinkman" }
+dict_3 = { "id": 2, "voornaam": "Merlijn", "achternaam": "Schaap" }
+dict_4 = { "id": 3, "voornaam": "Kevin", "achternaam": "Schuijn" } 
+
+mylist.append(dict_1)
+mylist.append(dict_2)
+mylist.append(dict_3)
+mylist.append(dict_4)
+print(mylist[1]["voornaam"], mylist[1]["achternaam"])
