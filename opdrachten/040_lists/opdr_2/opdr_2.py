f88b1de644b829a72c6ceb73e960256ad50b680f
pannekoek033 <keano2602@outlook.com>
2025-03-26 15:10:48 +0100

    opdracht af

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -1,6 +1,6 @@
 # Opdracht 2 lists
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 
 rivier_info = {
@@ -13,3 +13,11 @@ rivieren = list(rivier_info.keys())
 # rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
 # Hier jouw code.....
+
+print(f"De rivier {rivieren[0].capitalize()} stroomt onder andere door {rivier_info[rivieren[0]][1].capitalize()}")
+
+
+print(f"De rivier {rivieren[1].capitalize()} stroomt onder andere door {rivier_info[rivieren[0]][0].capitalize()}")
+
+
+print(f"De rivier {rivieren[2].capitalize()} stroomt onder andere door {rivier_info[rivieren[2]][2].capitalize()}")
