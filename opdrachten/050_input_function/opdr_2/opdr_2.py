a53c8c41b091cda19a73bb87fffe873c32de7cd8
pannekoek033 <keano2602@outlook.com>
2025-04-02 14:41:06 +0200

    opdracht af

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -1,7 +1,23 @@
 # Opdracht 2 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+gasten = ["Jij", "paul", "kees", "marie", "hilda"]
+gasten = tuple(gasten)
+gasten = list(gasten)
+
+print(gasten)
+
+#Nou belt Marie dat ze niet meer meegaat.
+
+del gasten[3]
+
+print(gasten)
+
+#Even later belt George, hij wil ook mee. George wil naast Kees zitten!
+
+gasten.insert(3, "George")
+
+print(gasten)
