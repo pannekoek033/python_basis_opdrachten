176712a5588ceb084a76b3ac5c0ab2237f65872e
pannekoek033 <keano2602@outlook.com>
2025-04-02 15:20:32 +0200

    opdracht af

--- opdrachten/060_loops/opdr_3/opdr_3.py
+++ opdrachten/060_loops/opdr_3/opdr_3.py
@@ -1,9 +1,14 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
 my_list = []
+
+for v in range(3, 82, 3):
+        my_list.append(v**2/3)
+
+print(my_list)
