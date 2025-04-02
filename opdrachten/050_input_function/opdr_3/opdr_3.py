3c9c70290a8bfefa6cdb47d97c66171fa5e4889d
pannekoek033 <keano2602@outlook.com>
2025-04-02 14:51:07 +0200

    opdracht af

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -1,6 +1,12 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 # Hier komt je code...
 
+steden = ["Amsterdam", "Nunspeet", "Nijkerk", "Amersfoort", "Zwolle"]
+steden = tuple(steden)
+steden = list(steden)
+
+steden.sort(reverse=True)
+print(steden)
