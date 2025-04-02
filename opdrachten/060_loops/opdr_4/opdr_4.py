45152b4287ffac9acdff61634eae2775eaf87bf3
pannekoek033 <keano2602@outlook.com>
2025-04-02 15:30:16 +0200

    opdracht af

--- opdrachten/060_loops/opdr_4/opdr_4.py
+++ opdrachten/060_loops/opdr_4/opdr_4.py
@@ -1,9 +1,13 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
-my_list = []
+my_list = [4*x + 7 for x in range(1, 11)]
+y = my_list
+
+
+print(y)
