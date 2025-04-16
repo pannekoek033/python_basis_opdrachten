5d6fb98e60d639b2e736e8a95b72b6a203b021ef
pannekoek033 <keano2602@outlook.com>
2025-04-16 14:47:14 +0200

    opdracht af

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -1,6 +1,28 @@
 # Opdracht 3 Tekst opslaan
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
+
+def encrypt_tekst(tekst, shift=5):
+    resultaat = ""
+
+    for char in tekst:
+        if char.isalpha():
+            basis = ord('A') if char.isupper() else ord('a')
+            verschoven = (ord(char) - basis + shift) % 26 + basis
+            resultaat += chr(verschoven)
+        else:
+            resultaat += char
+
+    return resultaat
+
+# Vraag de gebruiker om een tekst
+invoer = input("Geef de tekst die je wilt encrypten...\n")
+
+# Versleutel de tekst
+versleuteld = encrypt_tekst(invoer)
+
+# Print het resultaat
+print(versleuteld)
 
 
 
