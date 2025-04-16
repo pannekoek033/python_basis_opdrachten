0120813cee59f0986dd3d9c74f86549290d507d7
pannekoek033 <keano2602@outlook.com>
2025-04-16 14:58:19 +0200

    opdracht af

--- opdrachten/090_functies/opdr_4/opdr_4.py
+++ opdrachten/090_functies/opdr_4/opdr_4.py
@@ -1,14 +1,24 @@
 # Opdracht 1 functies
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 
 def volledige_naam(lijst_met_namen):
-    # hier komt jouw code
-    # Het woordje pass mag je weghalen
-    pass
+    # Itereer over de lijst van namen
+    for naam in lijst_met_namen:
+        # Controleer of er een tussenvoegsel is
+        if naam["tussenvoegsel"]:
+            # Als er een tussenvoegsel is, voeg dit toe
+            volledige_naam = f"{naam['voornaam']} {naam['tussenvoegsel']} {naam['achternaam']}"
+        else:
+            # Als er geen tussenvoegsel is, voeg alleen voornaam en achternaam toe
+            volledige_naam = f"{naam['voornaam']} {naam['achternaam']}"
+        
+        # Print de volledige naam
+        print(volledige_naam)
 
 
+# Test de functie met de lijst van namen
 namen = [
     {"voornaam": "Willem", "tussenvoegsel": "van", "achternaam": "Dijk"},
     {"voornaam": "Klaas", "tussenvoegsel": "", "achternaam": "Wopstra"},
