6ea3b24ebfe7dcd9b9330b0e709095e77e8a37ea
pannekoek033 <keano2602@outlook.com>
2025-04-16 14:34:04 +0200

    opdracht af

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -1,9 +1,17 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
-my_list = []
+pizzas = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+
+pizzas.sort()
+pizzas.append('yo-favorito')
+pizzas.remove('olivio')
+
+print(pizzas[:3])                      # eerste 3 pizza's
+print([pizzas[len(pizzas)//2]])       # middelste pizza
+print(pizzas[-3:])                    # laatste 3 pizza's
