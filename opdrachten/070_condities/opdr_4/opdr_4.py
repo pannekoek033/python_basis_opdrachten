3abe75b5e70823970ae5373c12c9ee20beae9e46
pannekoek033 <keano2602@outlook.com>
2025-04-16 14:41:50 +0200

    opdracht af

--- opdrachten/070_condities/opdr_4/opdr_4.py
+++ opdrachten/070_condities/opdr_4/opdr_4.py
@@ -1,12 +1,21 @@
 # Opdracht 4 condities
-# Naam student:
-# Groep:
+# Naam student: Keano Hotzky
+# Groep: IT2B
 
 
 
 toppings = [("olijven", 4.50), ("kaas", 3.50), ("salami", 3.00), ("pepperoni", 2.00) , ("ansjovis", 2.50)]
-beschikbare_toppings = ...
+beschikbare_toppings = [t[0] for t in toppings]
 
-keuze = input(f"Maak een keuze uit onze toppings: {beschikbare_toppings} \n")
 
-# Hier de rest van jouw code...
+keuze = input(f"Maak een keuze uit onze toppings: {beschikbare_toppings}\n")
+
+gevonden = False
+for naam, prijs in toppings:
+    if keuze == naam:
+        print(f"U heeft {naam} besteld. Dat kost {prijs}")
+        gevonden = True
+        break
+
+if not gevonden:
+    print("Uw keuze zit niet in ons assortiment")
